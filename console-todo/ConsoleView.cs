3e516a9c92329dd06162361e86db0c1d565ba1a6
Jan Suchotzki <jan@lernmoment.de>
2020-05-20 11:49:37 +0200

    Verbessert Separation of Concerns
    als Vorbereitung für #2

---
+++ console-todo/ConsoleView.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace console_todo
+{
+    static class ConsoleView
+    {
+        public static UserActivities GetNextActivityFromUser()
+        {
+            UserActivities result = UserActivities.Invalid;
+            bool isUserInputValid = false;
+
+            while (!isUserInputValid)
+            {
+                string userSelection = GetUserInput();
+                try
+                {
+                    result = ExtractUserActivity(userSelection);
+                    isUserInputValid = true;
+                }
+                catch (NotImplementedException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+            return result;
+        }
+
+        public static string GetTodoNameFromUser()
+        {
+            Console.Write("Bitte gib eine Aufgabe ein: ");
+            return Console.ReadLine();
+        }
+
+        private static string GetUserInput()
+        {
+            Console.WriteLine("### TODO-APP ###");
+            Console.WriteLine("Was möchtest du als nächstes machen?");
+            Console.WriteLine("1 - Neues ToDo hinzufügen");
+            Console.WriteLine("2 - Top 3 Todos anzeigen");
+            Console.Write("Deine Auswahl (1 oder 2): ");
+
+            return Console.ReadLine();
+        }
+
+        private static UserActivities ExtractUserActivity(string input)
+        {
+            UserActivities result;
+
+            switch (input)
+            {
+                case "1":
+                    result = UserActivities.AddTodo;
+                    break;
+                case "2":
+                    result = UserActivities.ListTopTodos;
+                    break;
+                default:
+                    throw new NotImplementedException($"Deine Eingabe ('{input}') wird momentan nicht unterstützt!");
+            }
+
+            return result;
+        }
+    }
+}
--- console-todo/Program.cs
+++ console-todo/Program.cs
@@ -15,13 +15,15 @@ namespace console_todo
         static void Main(string[] args)
         {
             List<Todo> todos = new List<Todo>();
-            UserActivities nextActivity = GetNextActivityFromUser();
+            UserActivities nextActivity = ConsoleView.GetNextActivityFromUser();
 
             switch (nextActivity)
             {
                 case UserActivities.AddTodo:
-                    AddNewTodo(todos);
+                    string todoName = ConsoleView.GetTodoNameFromUser();
+                    todos.Add(new Todo(todoName));
                     break;
+
                 case UserActivities.ListTopTodos:
                     break;
                 case UserActivities.Invalid:
@@ -29,66 +31,6 @@ namespace console_todo
                 default:
                     break;
             }
-        }
-
-        private static void AddNewTodo(List<Todo> todos)
-        {
-            Console.Write("Bitte gib eine Aufgabe ein: ");
-            string todoName = Console.ReadLine();
-
-            todos.Add(new Todo(todoName));
-        }
-
-        static UserActivities GetNextActivityFromUser()
-        {
-            UserActivities result = UserActivities.Invalid;
-            bool isUserInputValid = false;
-
-            while (!isUserInputValid)
-            {
-                string userSelection = GetUserInput();
-                try
-                {
-                    result = ExtractUserActivity(userSelection);
-                    isUserInputValid = true;
-                }
-                catch (NotImplementedException ex)
-                {
-                    Console.WriteLine(ex.Message);
-                }
-            }
-
-            return result;
-        }
-
-        private static string GetUserInput()
-        {
-            Console.WriteLine("### TODO-APP ###");
-            Console.WriteLine("Was möchtest du als nächstes machen?");
-            Console.WriteLine("1 - Neues ToDo hinzufügen");
-            Console.WriteLine("2 - Top 3 Todos anzeigen");
-            Console.Write("Deine Auswahl (1 oder 2): ");
-
-            return Console.ReadLine();
-        }
-
-        static UserActivities ExtractUserActivity(string input)
-        {
-            UserActivities result;
-
-            switch (input)
-            {
-                case "1":
-                    result = UserActivities.AddTodo;
-                    break;
-                case "2":
-                    result = UserActivities.ListTopTodos;
-                    break;
-                default:
-                    throw new NotImplementedException($"Deine Eingabe ('{input}') wird momentan nicht unterstützt!");
-            }
-
-            return result;
         }
     }
 }
